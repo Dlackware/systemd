dec175d8cc6561f6419637c754c049908438d84b
Bart van der Hall <bartgymnast@omgwtfroflol.com>
2014-06-01 16:31:33 +0200

    systemd and dbus updates

--- systemd/doinst.sh
+++ systemd/doinst.sh
@@ -11,74 +11,83 @@ config() {
 }
 
 # Keep same perms on rc.udev.new:
-if [ -r etc/rc.d/rc.udev -a -r etc/rc.d/rc.udev.new ]; then
-  chmod --reference=etc/rc.d/rc.udev etc/rc.d/rc.udev.new
+if [ -r /etc/rc.d/rc.udev -a -r /etc/rc.d/rc.udev.new ]; then
+  chmod --reference=/etc/rc.d/rc.udev /etc/rc.d/rc.udev.new
 fi
 
 ## List of conf files to check. The conf files in your package should end in .new
 
-config etc/rc.d/rc.local_shutdown.new
-config etc/rc.d/rc.udev.new
-config etc/vconsole.conf.new
-config etc/locale.conf.new
-config etc/machine-id.new
-config etc/machine-info.new
-config var/lib/systemd/catalog/database.new
-config etc/systemd/bootchart.conf.new
-config etc/systemd/journald.conf.new
-config etc/systemd/logind.conf.new
-config etc/systemd/system.conf.new
-config etc/systemd/user.conf.new
+config /etc/rc.d/rc.local_shutdown.new
+config /etc/rc.d/rc.udev.new
+config /etc/vconsole.conf.new
+config /etc/locale.conf.new
+config /etc/machine-id.new
+config /etc/machine-info.new
+config /var/lib/systemd/catalog/database.new
+config /etc/systemd/bootchart.conf.new
+config /etc/systemd/journald.conf.new
+config /etc/systemd/logind.conf.new
+config /etc/systemd/system.conf.new
+config /etc/systemd/user.conf.new
 
 function free_user_id {
   # Find a free user-ID >= 100 (should be < 1000 so it's not a normal user)
-  local FREE_USER_ID=100
-  while grep --quiet "^.*:.*:${FREE_USER_ID}:.*:.*:.*:" etc/passwd; do
+  local FREE_USER_ID=120
+  while grep --quiet "^.*:.*:${FREE_USER_ID}:.*:.*:.*:" /etc/passwd ; do
     let FREE_USER_ID++
   done
   echo ${FREE_USER_ID}
 }
+
 function free_group_id {
   # Find a free group-ID >= 120 (should be < 1000 so it's not a normal group)
   local FREE_GROUP_ID=120
-  while grep --quiet "^.*:.*:${FREE_GROUP_ID}:" etc/group; do
+  while grep --quiet "^.*:.*:${FREE_GROUP_ID}:" /etc/group ; do
     let FREE_GROUP_ID++
   done
   echo ${FREE_GROUP_ID}
 }
 
 # Set up groups.
-if ! grep --quiet '^lock:' etc/group ;then
-  chroot . /usr/sbin/groupadd \
+if ! grep --quiet '^lock:' /etc/group ;then
+  /usr/sbin/groupadd \
     -g $(free_group_id) \
     lock 2> /dev/null
 fi
-if ! grep --quiet '^systemd-journal:' etc/group ;then
-  chroot . /usr/sbin/groupadd \
+
+if ! grep --quiet '^systemd-journal:' /etc/group ;then
+  /usr/sbin/groupadd \
     -g $(free_group_id) \
     systemd-journal 2> /dev/null
 fi
-if ! grep --quiet '^systemd-journal-gateway:' etc/group ;then
-  chroot . /usr/sbin/groupadd \
+
+if ! grep --quiet '^systemd-journal-gateway:' /etc/group ;then
+  /usr/sbin/groupadd \
     -g $(free_group_id) \
     systemd-journal-gateway 2> /dev/null
 fi
 
+if ! grep --quiet '^systemd-timesync:' /etc/group ;then
+  /usr/sbin/groupadd \
+    -g $(free_group_id) \
+    systemd-timesync 2> /dev/null
+fi
+
 # Set up user: add it if it doesn't exist, update it if it already does.
-if OLD_ENTRY=$(grep --max-count=1 '^systemd-journal-gateway:' etc/passwd) \
+if OLD_ENTRY=$(grep --max-count=1 '^systemd-journal-gateway:' /etc/passwd) \
   || OLD_ENTRY=$(grep --max-count=1 \
-  ':/var/log/journal:[a-z/]*$' etc/passwd)
+  ':/var/log/journal:[a-z/]*$' /etc/passwd)
 then
   # Modify existing user
   OLD_USER=$(echo ${OLD_ENTRY} | cut --fields=1 --delimiter=':')
   USER_ID=$(echo ${OLD_ENTRY} | cut --fields=3 --delimiter=':')
   test ${USER_ID} -ge 1000 && USER_ID=$(free_user_id)
   if test "${OLD_USER}" = "systemd-journal-gateway"; then
-    echo -n "Updating unprivileged user" 1>&2
+    echo -n "Updating unprivileged user " 1>&2
   else
-    echo -ne "Changing unprivileged user \e[1m${OLD_USER}\e[0m to" 1>&2
+    echo -ne "Changing unprivileged user \e[1m${OLD_USER}\e[0m to " 1>&2
   fi
-  chroot . /usr/sbin/usermod \
+  /usr/sbin/usermod \
       -d '/var/log/journal' \
       -u ${USER_ID} \
       -s /bin/false \
@@ -86,8 +95,8 @@ then
       ${OLD_USER}
 else
   # Add new user
-  echo -n "Creating unprivileged user" 1>&2
-  chroot . /usr/sbin/useradd \
+  echo -n "Creating unprivileged user " 1>&2
+  /usr/sbin/useradd \
     -c 'Journal Gateway' \
     -u $(free_user_id) \
     -g systemd-journal-gateway \
@@ -96,64 +105,99 @@ else
     systemd-journal-gateway 2> /dev/null
 fi
 
+if OLD_ENTRY=$(grep --max-count=1 '^systemd-timesync:' /etc/passwd) \
+  || OLD_ENTRY=$(grep --max-count=1 \
+  ':/var/lib/systemd:[a-z/]*$' /etc/passwd)
+then
+  # Modify existing user
+  OLD_USER=$(echo ${OLD_ENTRY} | cut --fields=1 --delimiter=':')
+  USER_ID=$(echo ${OLD_ENTRY} | cut --fields=3 --delimiter=':')
+  test ${USER_ID} -ge 1000 && USER_ID=$(free_user_id)
+  if test "${OLD_USER}" = "systemd-timesync"; then
+    echo -n "Updating unprivileged user " 1>&2
+  else
+    echo -ne "Changing unprivileged user \e[1m${OLD_USER}\e[0m to " 1>&2
+  fi
+  /usr/sbin/usermod \
+      -d '/var/lib/systemd' \
+      -u ${USER_ID} \
+      -s /bin/false \
+      -g systemd-timesync \
+      ${OLD_USER}
+else
+  # Add new user
+  echo -n "Creating unprivileged user " 1>&2
+  /usr/sbin/useradd \
+    -c 'Systemd Timesync' \
+    -u $(free_user_id) \
+    -g systemd-timesync \
+    -s /bin/false \
+    -d '/var/lib/systemd' \
+    systemd-timesync 2> /dev/null
+fi
+
 setcaps () {
-  if chroot . /sbin/setcap "${1}" "${3}" 2>/dev/null; then
-    chroot . /bin/chmod "${2}" "${3}"
+  if /sbin/setcap "${1}" "${3}" 2>/dev/null; then
+    /bin/chmod "${2}" "${3}"
   fi
 }
 
 enableservice () {
-  if ! chroot . /bin/systemctl is-enable "${1}" > /dev/null 2>&1 ;then
-    chroot . /bin/systemctl enable "${1}" > /dev/null 2>&1
+  if ! /bin/systemctl is-enable "${1}" > /dev/null 2>&1 ;then
+    /bin/systemctl enable "${1}" 2>&1
   fi
 }
 
-# Try to read default runlevel from the old inittab if it exists
-runlevel=$(chroot . /bin/awk -F':' '$3 == "initdefault" && $1 !~ "^#" { print $2 }' /etc/inittab 2> /dev/null)
-if [ -z "${runlevel}" ]; then
-  target="/lib/systemd/system/graphical.target"
-else
-  target="/lib/systemd/system/runlevel${runlevel}.target"
-fi
-
 # And symlink what we found to the new-style default.target
-chroot . /bin/ln -sf "${target}" /etc/systemd/system/default.target
+ln -sf /lib/systemd/system/multi-user.target /etc/systemd/system/default.target
 
-if [ -r lib/systemd/systemd ]; then
-  mv -f lib/systemd/systemd lib/systemd/systemd.old
+if [ -r /lib/systemd/systemd ]; then
+  mv -f /lib/systemd/systemd /lib/systemd/systemd.old
 fi
 
-mv -f lib/systemd/systemd.new lib/systemd/systemd
+mv -f /lib/systemd/systemd.new /lib/systemd/systemd
 
-if [ -f lib/systemd/systemd.old ]; then
-  rm -f lib/systemd/systemd.old
+if [ -f /lib/systemd/systemd.old ]; then
+  rm -f /lib/systemd/systemd.old
+fi
+
+if [ ! -r /etc/systemd/system/syslog.service ] ;then
+  ln -s /lib/systemd/system/rsyslog.service /etc/systemd/system/syslog.service >/dev/null 2>&1 || :
 fi
 
 enableservice getty@tty1.service || :
 enableservice remote-fs.target || :
 enableservice systemd-readahead-replay.service || :
 enableservice systemd-readahead-collect.service || :
+enableservice systemd-networkd.service || :
 
 setcaps 'cap_dac_override,cap_sys_ptrace+ep' 'u-s' /usr/bin/systemd-detect-virt
 
-chroot . /bin/systemd-machine-id-setup > /dev/null 2>&1 || :
-chroot . /lib/systemd/systemd-random-seed save >/dev/null 2>&1 || :
-chroot . /bin/systemctl daemon-reexec > /dev/null 2>&1 || :
+/bin/systemd-machine-id-setup > /dev/null 2>&1 || :
+/lib/systemd/systemd-random-seed save >/dev/null 2>&1 || :
+/bin/systemctl daemon-reexec > /dev/null 2>&1 || :
 sleep 1
 
-chroot . /bin/systemctl stop systemd-udev.service systemd-udev-control.socket systemd-udev-kernel.socket >/dev/null 2>&1 || :
-chroot . /bin/systemctl --system daemon-reload >/dev/null 2>&1 || :
-chroot . /bin/systemctl start systemd-udev.service >/dev/null 2>&1 || :
-chroot . /sbin/udevadm hwdb --update >/dev/null 2>&1 || :
-chroot . /bin/journalctl --update-catalog >/dev/null 2>&1 || :
+/bin/systemctl stop systemd-udev.service systemd-udev-control.socket systemd-udev-kernel.socket >/dev/null 2>&1 || :
+/bin/systemctl --system daemon-reload >/dev/null 2>&1 || :
+/bin/systemctl start systemd-udev.service >/dev/null 2>&1 || :
+/sbin/udevadm hwdb --update >/dev/null 2>&1 || :
+/bin/journalctl --update-catalog >/dev/null 2>&1 || :
+/bin/systemd-tmpfiles --create >/dev/null 2>&1 || :
 
-if [ -f etc/nsswitch.conf ] ; then
-  chroot . sed -i.bak -e '
+if [ -f /etc/nsswitch.conf ] ; then
+  sed -i.bak -e '
     /^hosts:/ !b
     /\<myhostname\>/ b
     s/[[:blank:]]*$/ myhostname/
     ' /etc/nsswitch.conf >/dev/null 2>&1 || :
 fi
+
+# Make sure new journal files will be owned by the "systemd-journal" group
+/bin/chgrp systemd-journal /var/log/journal/ >/dev/null 2>&1 || :
+/bin/chgrp systemd-journal /var/log/journal/$(cat /etc/machine-id) >/dev/null 2>&1 || :
+/bin/chmod g+s /var/log/journal/ >/dev/null 2>&1 || :
+/bin/chmod g+s /var/log/journal/$(cat /etc/machine-id) >/dev/null 2>&1 || :
 
 /usr/bin/setfacl -Rnm g:adm:rx,d:g:adm:rx /var/log/journal/ >/dev/null 2>&1 || :
 
