9ec5db4afda38ffef466dcc316ea8b8fb4b55d97
Bart van der Hall <bartgymnast@hotmail.com>
2014-11-23 17:17:29 +0100

    enable services for runlevel4

--- l/gdk-pixbuf2/doinst.sh
+++ l/gdk-pixbuf2/doinst.sh
@@ -1,5 +1,7 @@
 chroot . /usr/bin/update-gdk-pixbuf-loaders 1> /dev/null 2> /dev/null
 
+systemctl enable update-gdk-pixbuf-loaders.service
+
 if [ -x bin/systemctl ] ; then
   chroot . /bin/systemctl --system daemon-reload >/dev/null 2>&1
 fi
--- l/pango/doinst.sh
+++ l/pango/doinst.sh
@@ -1,5 +1,8 @@
 # Updating the pango.modules file:
 chroot . /sbin/ldconfig 2> /dev/null
+
+systemctl enable update-pango-querymodules.service
+
 if [ -x /usr/bin/update-pango-querymodules ]; then
   /usr/bin/update-pango-querymodules
 fi
