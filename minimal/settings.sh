c067a041277a3b94a8a3811a7bdc91436ff33667
Bart van der Hall <bartgymnast@hotmail.com>
2015-02-02 13:56:28 +0100

    Updated ffmpeg to 2.5 branch and updated settings file

--- minimal/settings.sh
+++ minimal/settings.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-SBO_SOURCE=${SBO_SOURCE:-YES}
+# The following settings is for use if you build everything from source and is a required setting
+# SBO_SOURCE=${SBO_SOURCE:-YES}
 
+# Required:
 #
 # Make sure the /tmp directory is atleast 4GB
 # The below line needs to be replaced with a check and add or alter size
@@ -10,5 +12,9 @@ SBO_SOURCE=${SBO_SOURCE:-YES}
 
 echo 'tmpfs   /tmp         tmpfs   nodev,nosuid,size=4G          0  0' >> /etc/fstab || exit 1
 
-# echo 'ssh' >> /etc/securetty || exit 1
+# Optional:
+#
+# Allow root login on ssh
+
+echo 'ssh' >> /etc/securetty || exit 1
 
