b864a6ce91e0f345a863a1869d4f98cde5181d33
Bart van der Hall <bartgymnast@hotmail.com>
2015-02-21 21:22:55 +0100

    updates

--- minimal/systemd/apply-patches.sh
+++ minimal/systemd/apply-patches.sh
@@ -28,9 +28,8 @@ patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-microsoft-3000-keymap.pa
 # From Slackware
 patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/60-cdrom_id.rules.diff
 
-# Upstream
-ApplyPatch 0001-use-correct-format-types.patch
-
+# Extra
+ApplyPatch fedora-disable-resolv.conf-symlink.patch
 ### Arch
 
 # Set to YES if autogen is needed
