1434bcdbd5a4e7dcd7e5549634a60c9d75ac8677
Bart van der Hall <bartgymnast@hotmail.com>
2015-10-31 12:41:06 +0100

    Update for 3.18

--- systemd/systemd/apply-patches.sh
+++ systemd/systemd/apply-patches.sh
@@ -28,10 +28,6 @@ patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-microsoft-3000-keymap.pa
 # From Slackware
 patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/60-cdrom_id.rules.diff
 
-# Extra
-ApplyPatch fedora-disable-resolv.conf-symlink.patch
-### Arch
-
 # Set to YES if autogen is needed
 SBO_AUTOGEN=YES
 
