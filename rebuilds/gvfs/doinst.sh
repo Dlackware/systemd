ebd76a5634862f53bdc932565529bc3e7101e0b2
Bart van der Hall <bartgymnast@gmail.com>
2016-04-13 11:17:18 +0200

    fixed gnome-keyring pam on 64bit, added libbluray and recompiled gvfs with bluray support

--- rebuilds/gvfs/doinst.sh
+++ rebuilds/gvfs/doinst.sh
@@ -1,17 +1,4 @@
-# Reload .mount files:
-PID=$(/sbin/pidof -o %PPID gvfsd)
-if [ -n "${PID}" ] ;then
-  kill -USR1 ${PID} >&/dev/null
-fi
+# Try to run these.  If they fail, no biggie.
+chroot . /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas/ 1> /dev/null 2> /dev/null
+chroot . /usr/bin/gio-querymodules @LIBDIR@/gio/modules/ 1> /dev/null 2> /dev/null
 
-if [ -x usr/bin/update-desktop-database ]; then
-  chroot . /usr/bin/update-desktop-database 1> /dev/null 2> /dev/null
-fi
-
-if [ -x usr/bin/gio-querymodules ] ; then
-  chroot . /usr/bin/gio-querymodules @LIBDIR@/gio/modules &> /dev/null
-fi
-
-if [ -x /usr/bin/glib-compile-schemas ] ;then
-  chroot . /usr/bin/glib-compile-schemas /usr/share/glib-2.0/schemas &> /dev/null
-fi
