0da999d5ef8f519b853dfde1b28f73de8bddce9e
Bart van der Hall <bartgymnast@gmail.com>
2016-04-21 13:31:27 +0200

    Updated gst plugins to 1.6.4, NetworkManager to 1.2.0, mesa to 1.2.1, shadow a doinst.sh update

--- systemd/shadow/doinst.sh
+++ systemd/shadow/doinst.sh
@@ -4,22 +4,17 @@ config() {
   # If there's no config file by that name, mv it over:
   if [ ! -r $OLD ]; then
     mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then # toss the redundant copy
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
     rm $NEW
   fi
   # Otherwise, we leave the .new copy for the admin to consider...
 }
-
-for i in etc/pam.d/*.new ; do
-    config $i
-done
 
 config etc/default/useradd.new
 
 config var/log/faillog.new
 rm -f var/log/faillog.new
 
-mv /etc/login.defs /etc/login.defs.old
+cp -f /etc/login.defs /etc/login.defs.old
 mv /etc/login.defs.new /etc/login.defs
-
-#config etc/default.useradd.new
