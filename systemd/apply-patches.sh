79c08b298867aac1d3c8cce0dd07d0c04d1b46de
Bart van der Hall <bartgymnast@gmail.com>
2017-03-01 23:55:52 +0100

    Updated systemd

--- systemd/apply-patches.sh
+++ systemd/apply-patches.sh
@@ -24,10 +24,10 @@ ApplyPatch() {
 }
 
 # patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/${NAME}.patch
-patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/netdev.patch
-patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/gperf31.patch
+#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/netdev.patch
+#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/gperf31.patch
 patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-microsoft-3000-keymap.patch
-patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-rootlibdir.patch
+#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-rootlibdir.patch
 # From Slackware
 patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/60-cdrom_id.rules.diff
 
