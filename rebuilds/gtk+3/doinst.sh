4bb110942ec4d63e2e045f88d0fe345a70eb9870
Eugen Wissner <belka@caraus.de>
2017-08-10 06:45:29 +0200

    gtk+3: Updated for 3.22.18

--- rebuilds/gtk+3/doinst.sh
+++ rebuilds/gtk+3/doinst.sh
@@ -9,11 +9,8 @@ if [ -x usr/bin/glib-compile-schemas ] ;then
   chroot . /usr/bin/glib-compile-schemas --allow-any-name /usr/share/glib-2.0/schemas &> /dev/null
 fi
 
-chroot . systemctl enable update-gtk-immodules-2.service
 chroot . systemctl enable update-gtk-immodules-3.service
 
 if [ -x bin/systemctl ] ; then
   chroot . /bin/systemctl --system daemon-reload >/dev/null 2>&1
 fi
-
-
