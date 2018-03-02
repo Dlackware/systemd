cd1eb65678919e9212746e4e8a7977970284ed28
Bart van der Hall <bartgymnast@hotmail.com>
2018-03-02 10:55:31 +0100

    systemd: Updated to v237

--- systemd/apply-patches.sh
+++ systemd/apply-patches.sh
@@ -24,10 +24,8 @@ ApplyPatch() {
 }
 
 # patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/${NAME}.patch
-#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/netdev.patch
-#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/gperf31.patch
+#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/glibc227.patch
 patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-microsoft-3000-keymap.patch
-#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-rootlibdir.patch
 # From Slackware
 patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/60-cdrom_id.rules.diff
 
@@ -35,6 +33,6 @@ patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/Don-t-enable-audit-by-default
 
 
 # Set to YES if autogen is needed
-SBO_AUTOGEN=NO
+SBO_AUTOGEN=YES
 
 set +e +o pipefail
