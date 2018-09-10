67225ee5445bbceb31f69a0eaaf93b08406c1261
Bart van der Hall <bartgymnast@gmail.com>
2018-09-10 19:56:58 +0200

    systemd: Updated to v239

--- systemd/apply-patches.sh
+++ systemd/apply-patches.sh
@@ -25,8 +25,8 @@ ApplyPatch() {
 
 # patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/${NAME}.patch
 patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-microsoft-3000-keymap.patch
-patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/libmount.patch
-patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/gpg-error.patch
+#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/libmount.patch
+#patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/gpg-error.patch
 # From Slackware
 patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/60-cdrom_id.rules.diff
 patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/Don-t-enable-audit-by-default.patch
