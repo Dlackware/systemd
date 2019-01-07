4dcc81412be946e4a33ad934b129b08ddd56d581
Eugen Wissner <belka@caraus.de>
2019-01-07 06:13:01 +0100

    systemd: Update for version 240

--- systemd/apply-patches.sh
+++ systemd/apply-patches.sh
@@ -1,10 +1,5 @@
-SBO_PATCHDIR=${CWD}/patches
-
-# patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/${NAME}.patch
-patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/udev-microsoft-3000-keymap.patch
-patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/meson-options-debug.patch
-patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/detect-statx.patch
+patch -p0 -E --backup --verbose -i $CWD/patches/udev-microsoft-3000-keymap.patch
 
 # From Slackware
-patch -p0 -E --backup --verbose -i ${SBO_PATCHDIR}/60-cdrom_id.rules.diff
-patch -p1 -E --backup --verbose -i ${SBO_PATCHDIR}/Don-t-enable-audit-by-default.patch
+patch -p0 -E --backup --verbose -i $CWD/patches/60-cdrom_id.rules.diff
+patch -p1 -E --backup --verbose -i $CWD/patches/Don-t-enable-audit-by-default.patch
