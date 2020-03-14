7495626328d0e5645556b16c33a3bf1bcd6121af
Eugen Wissner <belka@caraus.de>
2020-03-14 16:48:56 +0100

    systemd: Update for version 245
    Fix PAM files directory /lib instead of /usr/lib.
    Update patches.

--- systemd/apply-patches.sh
+++
@@ -1,5 +0,0 @@
-patch -p0 -E --backup --verbose -i $CWD/patches/udev-microsoft-3000-keymap.patch
-
-# From Slackware
-patch -p0 -E --backup --verbose -i $CWD/patches/60-cdrom_id.rules.diff
-patch -p1 -E --backup --verbose -i $CWD/patches/Don-t-enable-audit-by-default.patch
